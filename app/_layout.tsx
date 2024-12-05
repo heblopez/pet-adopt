d6a68c3057229fd5a40531a0289e2991be12a8fb
Heberth López <heberth.lopez.19@gmail.com>
2024-12-05 13:29:21 -0500

    feat: :sparkles: Integrate ClerkProvider with token caching and error handling in RootLayout for authentication support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,42 @@
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was used 🔐 \n`);
+      } else {
+        console.log("No values stored under key: " + key);
+      }
+      return item;
+    } catch (error) {
+      console.error("SecureStore get item error: ", error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export default function RootLayout() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  if (!publishableKey) {
+    throw new Error(
+      "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+    );
+  }
+
   const [loaded] = useFonts({
     "Pally-Regular": require("../assets/fonts/Pally-Regular.otf"),
     "Pally-Bold": require("../assets/fonts/Pally-Bold.otf"),
@@ -16,9 +51,11 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login/index" options={{ headerShown: false }} />
-    </Stack>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login/index" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
   );
 }
