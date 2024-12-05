46dc5c380b2aec2f635536a3c5131784647dad7b
Heberth López <heberth.lopez.19@gmail.com>
2024-12-05 12:59:21 -0500

    feat: :sparkles: Introduce color constants for consistent theming across the app

---
+++ constants/Colors.ts
@@ -0,0 +1,10 @@
+export default {
+  primary: "#1f8fff",
+  secondary: "#00bfff",
+  background: "#faf8f6",
+  foreground: "#85cdfa",
+  accent: "#add8e6",
+  light: "#f0f8ff",
+  gray: "#536574",
+  white: "#fff",
+};
