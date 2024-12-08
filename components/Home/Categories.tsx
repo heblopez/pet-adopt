2c377b8ee710f8ec568b7c5111d3eb8c58396aff
Heberth López <heberth.lopez.19@gmail.com>
2024-12-07 22:01:08 -0500

    feat: :sparkles: Move pet data to a separate file for better organization and import it into Categories component

--- components/Home/Categories.tsx
+++ components/Home/Categories.tsx
@@ -9,45 +9,7 @@ import {
   FlatList,
 } from "react-native";
 import PetItem from "./PetItem";
-
-const dataPets = [
-  {
-    name: "Zeus",
-    category: "Dogs",
-    imageUrl:
-      "https://spotpet.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fm5ehn3s5t7ec%2Fwp-image-197858%2F7746c729ddf5a2a730f17999268362de%2FLabrador-Retriever-Dog-Breed-Guide.jpg&w=1200&q=75",
-    breed: "Labrador Retriever",
-    age: 4,
-    address: "777 Main St, New York, NY 10010",
-  },
-  {
-    name: "Figaro",
-    category: "Cats",
-    imageUrl:
-      "https://spotpet.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fm5ehn3s5t7ec%2Fwp-image-197219%2Fadadb5218e0bc3445222ef21e8e931d5%2FBengal-Cat-Breed-Guide.jpg&w=1200&q=75",
-    breed: "Bengal",
-    age: 3,
-    address: "123 North St, New York, NY 10010",
-  },
-  {
-    name: "Hamlet",
-    category: "Hamsters",
-    imageUrl:
-      "https://www.reptilecymru.co.uk/wp-content/uploads/2021/03/Syrian-Hamster-.png",
-    breed: "Syrian",
-    age: 1,
-    address: "456 Central Ave, Los Angeles, CA 90010",
-  },
-  {
-    name: "Nemo",
-    category: "Fish",
-    imageUrl:
-      "https://i.pinimg.com/736x/24/7f/ae/247fae125ccd5c0966848b67b3b2389b.jpg",
-    breed: "Clownfish",
-    age: 2,
-    address: "345 Ocean Ave, Miami, FL 33139",
-  },
-];
+import { dataPets } from "@/data/pets";
 
 export default function Categories() {
   const categories = [
---
+++ data/pets.ts
@@ -0,0 +1,56 @@
+export const dataPets = [
+  {
+    name: "Zeus",
+    category: "Dogs",
+    imageUrl:
+      "https://spotpet.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fm5ehn3s5t7ec%2Fwp-image-197858%2F7746c729ddf5a2a730f17999268362de%2FLabrador-Retriever-Dog-Breed-Guide.jpg&w=1200&q=75",
+    breed: "Labrador Retriever",
+    age: 4,
+    address: "777 Main St, New York, NY 10010",
+  },
+  {
+    name: "Figaro",
+    category: "Cats",
+    imageUrl:
+      "https://spotpet.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fm5ehn3s5t7ec%2Fwp-image-197219%2Fadadb5218e0bc3445222ef21e8e931d5%2FBengal-Cat-Breed-Guide.jpg&w=1200&q=75",
+    breed: "Bengal",
+    age: 3,
+    address: "123 North St, New York, NY 10010",
+  },
+  {
+    name: "Hamlet",
+    category: "Hamsters",
+    imageUrl:
+      "https://www.reptilecymru.co.uk/wp-content/uploads/2021/03/Syrian-Hamster-.png",
+    breed: "Syrian",
+    age: 1,
+    address: "456 Central Ave, Los Angeles, CA 90010",
+  },
+  {
+    name: "Nemo",
+    category: "Fish",
+    imageUrl:
+      "https://i.pinimg.com/736x/24/7f/ae/247fae125ccd5c0966848b67b3b2389b.jpg",
+    breed: "Clownfish",
+    age: 2,
+    address: "345 Ocean Ave, Miami, FL 33139",
+  },
+  {
+    name: "Benji",
+    category: "Dogs",
+    imageUrl:
+      "https://spotpet.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fm5ehn3s5t7ec%2F1KMfTfANxKqacv6gGIuDl9%2Fd71041bfb9dfa67682ea899dad339d97%2FJack_Russell_Terrier_Price.jpeg&w=1080&q=75",
+    breed: "Jack Russel Terrier",
+    age: 2,
+    address: "369 Atlantic St, New York, NY 10010",
+  },
+  {
+    name: "Rex",
+    category: "Dogs",
+    imageUrl:
+      "https://images.squarespace-cdn.com/content/v1/5e7d1a6f18b2b26e9726b89f/1619819693082-F42T8XJ8Q1BXGYDL0EY9/saying+goodbye+to+your+dog",
+    breed: "German Shepherd",
+    age: 1,
+    address: "789 Broadway St, New York, NY 10010",
+  },
+];
