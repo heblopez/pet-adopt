c5eed08bb35985982dc203e1590f0a9eb043f19f
Heberth López <heberth.lopez.19@gmail.com>
2024-12-16 01:46:06 -0500

    feat: :sparkles: Add functionality to manage favorite pets by implementing newFavoritePet and deleteFavoritePet services

--- services/favorite.service.ts
+++ services/favorite.service.ts
@@ -26,3 +26,48 @@ export const getFavoritePets = async (token: string) => {
     return null;
   }
 };
+
+export const newFavoritePet = async (petId: number, token: string) => {
+  try {
+    const response = await fetch(`${API_FAVORITE_URL}/${petId}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to add favorite pet");
+    }
+    const data = await response.json();
+    const favParsed = {
+      ...data.data.pet,
+      userId: data.data.pet.owner.ownerId,
+      userFullName: `${data.data.pet.owner.firstName} ${data.data.pet.owner.lastName}`,
+      userEmail: data.data.pet.owner.email,
+      userImageUrl: data.data.pet.owner.avatarUrl,
+    };
+    return favParsed;
+  } catch (error) {
+    console.error("Error adding favorite pet: ", error);
+    return null;
+  }
+};
+
+export const deleteFavoritePet = async (petId: number, token: string) => {
+  try {
+    const response = await fetch(`${API_FAVORITE_URL}/${petId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to remove favorite pet");
+    }
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error("Error removing favorite pet: ", error);
+    return null;
+  }
+};
